7b07948c7651cd1b190680e4a2f1f9c6ad87c764
mccIsHere <zxj.comads@gmail.com>
2016-02-18 19:53:26 -0800

    Update zxj.py

--- 3sum-closest/zxj.py
+++ 3sum-closest/zxj.py
@@ -1,2 +1,23 @@
-Test
-hahaha
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        snums=sorted(nums)
+        llen=len(nums)
+        mx=None
+        md=None
+        for i0 in xrange(llen-2):
+            i1=i0+1
+            i2=llen-1
+            while i1<i2:
+                curx=snums[i0]+snums[i1]+snums[i2]
+                curd=abs(curx-target)
+                if md is None or curd<md:
+                    mx,md=curx,curd
+                if curx<target: i1+=1
+                elif curx>target: i2-=1
+                else: return target
+        return mx
