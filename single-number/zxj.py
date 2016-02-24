cfc6fa85a00cdcbbd96d3c02fea9790a82f183e4
mccIsHere <zxj.comads@gmail.com>
2016-02-23 21:33:03 -0800

    Create zxj.py

---
+++ single-number/zxj.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        x=0
+        for n in nums: x^=n
+        return x
